cd3dab4e780e6c6a5c7af2fbec0c34248cf39247
Mason Fidino <mfidino@lpzoo.org>
2023-11-01 16:44:24 -0500

    plotted some initial results

--- R/simulation_utils.R
+++ R/simulation_utils.R
@@ -27,3 +27,37 @@ fit_simulations <- function(data, ncores = 10, nsim){
   parallel::stopCluster(cl)
   return(result)
 }
+
+rmse <- function(xi, x){
+  tmp <- sum(
+    (xi - x)^2
+  ) / length(xi)
+  tmp <- sqrt(tmp)
+  return(tmp)
+}
+
+calc_rmse <- function(one_coef, truth){
+  cnames <- unique(one_coef$parameter)
+  tmp <- data.frame(
+    parameter = cnames,
+    rmse = NA,
+    coverage = NA
+  )
+  tpars <- c(
+    truth$psi, truth$theta, truth$rho
+  )
+  for(i in 1:nrow(tmp)){
+    tmp_coef <- one_coef[
+      one_coef$parameter == tmp$parameter[i],
+    ]
+    tmp$rmse[i] <- rmse(
+      tmp_coef$Est,
+      tpars[i]
+    )
+    tmp$coverage[i] <- mean(
+      tmp_coef$lower <= tpars[i] &
+        tmp_coef$upper >= tpars[i]
+    )
+  }
+  return(tmp)
+}
